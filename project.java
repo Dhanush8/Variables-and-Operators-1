3ae665cefbd28c789afafcaa23300c8d12a1b7bf
Dhanush8 <kasundanushka503@gmail.com>
2021-07-11 21:20:22 +0530

    Initial commit

---
+++ project.java
@@ -0,0 +1,20 @@
+package abc;
+
+import java.util.Scanner;
+
+public class Abc {
+
+    public static void main(String[] args) {
+
+        System.out.print("Enter your first name: ");
+        String firstName=sc.nextLine();
+
+        System.out.print("Enter your last name: ");
+        String lastName=sc.nextLine();
+
+        String fullName=firstName+lastName;
+
+        System.out.print("Your full name is: "+fullName);
+}
+
+}
